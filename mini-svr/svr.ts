345611b33dea8ddaa3a3f53f7de6fb554db92729
Martin Reid <mreid070@marriott.com>
2026-04-23 07:55:48 -0600

    new svr stuff

--- mini-svr/svr.ts
+++ mini-svr/svr.ts
@@ -1,7 +1,15 @@
-import express, { type Request, type Response } from 'express';
+import { readFileSync, writeFileSync } from 'node:fs'
+import validator from 'validator'
+import express, { type Request, type Response } from 'express'
+import { type RoomMap } from './types.ts'
 
 const port = 8080
+const ip   = "0.0.0.0"
 const svr  = express()
+
+const roomMapFileName = "valet_room_map.json"
+const roomMapRawInit  = readFileSync(roomMapFileName, {encoding: "utf-8"})
+let roomMap = JSON.parse(roomMapRawInit) as RoomMap
 
 function ping(req: Request, res: Response) {
     res.json({
@@ -11,6 +19,29 @@ function ping(req: Request, res: Response) {
     })
 }
 
+function sendRoomMap(_: Request, res: Response) {
+    res.json(roomMap)
+}
+
+function updateRoomMap(req: Request, res: Response) {
+    
+    const recvd = req.body
+    try {
+        roomMap = recvd as RoomMap
+        writeFileSync(roomMapFileName, JSON.stringify(roomMap, null, 4))
+        res.status(204).send("Valet/room map updated")
+    }
+    catch {
+        const serialized = JSON.stringify(recvd, null, 4)
+        res.status(400).send(`Invalid data format. Received: ${serialized}`)
+        return
+    }
+}
+
+svr.get("/", sendRoomMap)
+svr.put("/", updateRoomMap)
 svr.get("/ping", ping)
 
-svr.listen(port, () => console.log(`Server listening on port ${port}`))
+svr.use(express.json())
+
+svr.listen(port, ip, () => console.log(`Server listening on ${ip}:${port}`))
