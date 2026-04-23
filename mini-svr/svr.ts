e2ca5101cc0452a97553fad4947e1e4bfbafb801
Martin Reid <mreid070@marriott.com>
2026-04-23 10:37:17 -0600

    svr put works

--- mini-svr/svr.ts
+++ mini-svr/svr.ts
@@ -1,5 +1,5 @@
 import { readFileSync, writeFileSync } from 'node:fs'
-import validator from 'validator'
+import { Ajv } from 'ajv'
 import express, { type Request, type Response } from 'express'
 import { type RoomMap } from './types.ts'
 
@@ -8,8 +8,14 @@ const ip   = "0.0.0.0"
 const svr  = express()
 
 const roomMapFileName = "valet_room_map.json"
+const schemaFileName  = "room_map.schema.json"
+const schemaText      = readFileSync(schemaFileName, {encoding: "utf-8"})
 const roomMapRawInit  = readFileSync(roomMapFileName, {encoding: "utf-8"})
+const schema          = JSON.parse(schemaText)
+
 let roomMap = JSON.parse(roomMapRawInit) as RoomMap
+
+const schemaValidator = new Ajv()
 
 function ping(req: Request, res: Response) {
     res.json({
@@ -25,15 +31,15 @@ function sendRoomMap(_: Request, res: Response) {
 
 function updateRoomMap(req: Request, res: Response) {
     
-    const recvd = req.body
-    try {
-        roomMap = recvd as RoomMap
-        writeFileSync(roomMapFileName, JSON.stringify(roomMap, null, 4))
+    const recvd  = req.body
+    const serial = JSON.stringify(recvd, null, 4)
+
+    if (schemaValidator.validate(schema, recvd)) {
+        writeFileSync(roomMapFileName, serial)
         res.status(204).send("Valet/room map updated")
     }
-    catch {
-        const serialized = JSON.stringify(recvd, null, 4)
-        res.status(400).send(`Invalid data format. Received: ${serialized}`)
+    else {
+        res.status(400).send(`Invalid data. Received: ${serial}`)
         return
     }
 }
@@ -42,6 +48,6 @@ svr.get("/", sendRoomMap)
 svr.put("/", updateRoomMap)
 svr.get("/ping", ping)
 
-svr.use(express.json())
+svr.use(express.json())       // only parses bodies with Content-Type set to "application/json"
 
 svr.listen(port, ip, () => console.log(`Server listening on ${ip}:${port}`))
